4cc168f34a8c595a0c62b3211264fabd719a71b5
Renato Andrade <dev.renatopandrade@gmail.com>
2023-04-26 20:29:10 -0300

    initial setup

---
+++ src/main/java/com/randradee/spring/tutorial/school/SpringTutorialSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.randradee.spring.tutorial.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorialSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/randradee/spring/tutorial/school/SpringTutorialSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randradee.spring.tutorial.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
